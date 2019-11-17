6b15f1e443dd1a824191c1927aeb04fcb9af1da8
Ryszard Glegola <ryszard.glegola@gmail.com>
2019-11-17 09:12:29 +0100

    Updated titleClickHandler, now it also adds class ACTIVE to article link clicked.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 const titleClickHandler = function(event){
   	console.log('Link was clicked!',event);  // Do wykomentowania
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
 	const activeLinks = document.querySelectorAll('.titles a.active');
 	for(let activeLink of activeLinks){
 	  activeLink.classList.remove('active');
 	}
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+	
+	const clickedElement = this;
+    console.log('clickedElement:',clickedElement);
+    clickedElement.classList.add('active');
+    console.log('Article link' + clickedElement + 'is now active!');  // Do wykomentowania
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
 
 	const activeArticles = document.querySelectorAll('.posts article.active');
 	for(let activeArticle of activeArticles){
@@ -23,6 +28,7 @@ const titleClickHandler = function(event){
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
+
 }
 
 const links = document.querySelectorAll('.titles a');
