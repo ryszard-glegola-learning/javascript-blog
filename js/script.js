63768aca04a390ba530cb9c327656cf3966e19b9
Ryszard Glegola <ryszard.glegola@gmail.com>
2019-11-30 17:30:28 +0000

    Changed CSS to dark theme.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-const titleClickHandler = function(event){
-  	console.log('Link was clicked!',event);  // Do wykomentowania
+const titleClickHandler = function(event){   
+  
+  // Dlaczego titleClickHandler jest stala?
+  // console.log('Link was clicked!',event); 
 
   /* [DONE] remove class 'active' from all article links  */
-
 	const activeLinks = document.querySelectorAll('.titles a.active');
 	for(let activeLink of activeLinks){
 	  activeLink.classList.remove('active');
 	}
 
   /* [DONE] add class 'active' to the clicked link */
-	
 	event.preventDefault();
 	const clickedElement = this;
     console.log('clickedElement:',clickedElement);
@@ -17,7 +17,6 @@ const titleClickHandler = function(event){
     console.log('Article link ' + clickedElement + ' is now active!');  // Do wykomentowania
 
   /* [DONE] remove class 'active' from all articles */
-
 	const activeArticles = document.querySelectorAll('.posts article.active');
 	for(let activeArticle of activeArticles){
 	  activeArticle.classList.remove('active');
@@ -25,17 +24,13 @@ const titleClickHandler = function(event){
 	}  
 
   /* [DONE] get 'href' attribute from the clicked link */
-
   	const hrefAttributeClicked = clickedElement.getAttribute('href');
 
   /* find the correct article using the selector (value of 'href' attribute) */
-
   	const theArticleIamLookingFor = document.querySelector(hrefAttributeClicked);
 
   /* add class 'active' to the correct article */
-
    	theArticleIamLookingFor.classList.add('active');
-
 }
 
 const links = document.querySelectorAll('.titles a');
@@ -43,3 +38,51 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+/* script 'settings' in constants - good practice */
+const optArticleSelector = '.post',
+	  optTitleSelector = '.post-title',
+	  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+	/* remove contents of titleList */
+	const titleList = document.querySelector(optTitleListSelector);
+
+	// Dlaczego titleList to STALA, mimo ze potem zmieniamy jej wartosc?
+
+	// console.log('titleList: ',titleList);  
+	titleList.innerHTML = ''; // 
+
+	/* for each article */
+	const articles = document.querySelectorAll('.posts article.post');
+	// console.log('articles: ',articles);  
+
+  	let html = '';
+
+	for(let article of articles){
+
+		/* get the article id */
+		// console.log('article: ',article); 
+		const articleId = article.getAttribute('id');
+		// console.log('articleId: ',articleId); 
+
+		/* find the title element */
+		/* get the title from the title element */
+		const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+		// console.log('articleTitle: ',articleTitle); 
+
+		/* create HTML of the link */
+		const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+		// console.log('linkHTML: ',linkHTML);
+
+		/* insert link into html variable */
+		html = html + linkHTML;
+		// console.log('html: ',html);
+	}
+
+ 	titleList.innerHTML = html;
+
+}
+
+generateTitleLinks();
